68bdcbd80727fce7477331c8e63f1561451066bc
Henrique <yherique.yhenriquefilho@gmail.com>
2024-09-02 14:57:11 -0300

    02-09

---
+++ appmy/src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      
+      <header className="logo">
+        <a href="/" className="furacao">Logo</a>
+        <nav>
+        
+          <ul className="menu">
+            <li><a href="/" className="letras1">Sobre</a></li>
+            <li><a href="/" className="letras2">Produtos</a></li>
+            <li><a href="/" className="letras3">Cantato</a></li>
+          </ul>
+          <section className="flex">
+
+          </section>
+        </nav>
+      </header>
+      <div className="colun">
+
+<h1 className="colun1">
+  COLUNA
+</h1>
+
+      </div>
+
+    </div>
+  );
+}
+
+
+export default App;
